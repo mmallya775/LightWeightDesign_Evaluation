edd573b4c172c26c560d5acdc9ad4ed7a597651d
Mohit Radhesh Mallya <mmallya775@gmail.com>
2023-08-12 16:26:16 +0200

    All Graphs

--- CouponTest_Results.py
+++ CouponTest_Results.py
@@ -15,7 +15,7 @@ sns.set_theme(style="whitegrid")
 plt.figure(figsize=(7, 5), dpi=200)
 
 sns.lineplot(x='Traverse',y='Kraft',data=dataset1,label='0° Sample', color='red')
-sns.lineplot(x='Traverse',y='Kraft',data=dataset2,label='[0/90/45/-45] Sample', color='blue')
+sns.lineplot(x='Traverse',y='Kraft',data=dataset2,label='[0/90/45/-45]s Sample', color='blue')
 
 plt.xlabel(r"Displacement (mm) $\longrightarrow$")
 plt.ylabel(r"Load (N) $\longrightarrow$")
@@ -35,8 +35,27 @@ sns.set_theme(style="whitegrid")
 plt.figure(figsize=(7, 5), dpi=200)
 
 sns.lineplot(x='Strain',y='Stress',data=dataset1,label='0° Sample', color='red')
-sns.lineplot(x='Strain',y='Stress',data=dataset2,label='[0/90/45/-45] Sample', color='blue')
+sns.lineplot(x='Strain',y='Stress',data=dataset2,label='[0/90/45/-45]s Sample', color='blue')
 plt.xlabel(r"Strain $\longrightarrow$")
 plt.ylabel(r"Stress (MPa) $\longrightarrow$")
+plt.title("Stress-Strain Curves")
+plt.show()
+
+
+dataset3 = pd.read_excel(io="D:/OneDrive/Lightweight Design Lab Data/EXT Data/0deg_sp1.xlsx", header=0)
+dataset4 = pd.read_excel(io="D:/OneDrive/Lightweight Design Lab Data/EXT Data/Mix_Sp2.xlsx", header=0)
+
+print(dataset3.head(20))
+print("========================================")
+print(dataset4.head(20))
+
+sns.set_theme(style="whitegrid")
+plt.figure(figsize=(7,5), dpi=200)
+
+sns.lineplot(x='Strain',y='Stress',data=dataset3, color='red', label='0° Sample')
+sns.lineplot(x='Strain',y='Stress',data=dataset4, color='blue', label ='[0/90/45/-45]s Sample')
+plt.xlabel(r"Strain (mm/mm) $\longrightarrow$")
+plt.ylabel(r"Stress (MPa) $\longrightarrow$")
+plt.xlim(0,0.015)
 plt.title("Stress-Strain Curves")
 plt.show()
